49bd0dd832ba1c9f458d4165631d7319136a1805
Kushagra Nigam <31718607+kushagranigam@users.noreply.github.com>
2019-01-22 02:12:44 +0530

    Drive Page added: 13 June 2017

---
+++ Week2Module1/MyDrive.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MyDrive.aspx.cs" Inherits="Week2Module1.MyDrive" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ Week2Module1/MyDrive.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Week2Module1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        String currentPath = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            currentPath = "~/Directories/" + Session["handle"] + "/";
+            DirectoryInfo MyDirectory = new DirectoryInfo(currentPath);
+            DirectoryInfo[] MySub = MyDirectory.GetDirectories();
+            FileInfo[] MyFiles = MyDirectory.GetFiles();
+
+        }
+    }
+}
